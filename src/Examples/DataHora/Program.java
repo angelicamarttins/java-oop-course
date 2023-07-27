340f5dd7e76735dab8e5627754ae8c963a699048
Angélica Martins <marttinsangelica@gmail.com>
2023-07-27 19:26:40 -0300

    fea: continue dateTime class

--- src/Examples/DataHora/Program.java
+++ src/Examples/DataHora/Program.java
@@ -9,54 +9,103 @@ import java.time.format.DateTimeFormatter;
 
 public class Program {
   public static void main(String[] args) {
-    LocalDate localDate = LocalDate.now(); // Data local sem hora
+    // Data local sem hora
+    LocalDate localDate = LocalDate.now();
     System.out.println("localDate " + localDate);
 
-    LocalDate isoToLocalDate = LocalDate.parse("1991-11-19"); // De ISO para data local sem hora
+    // De ISO para data local sem hora
+    LocalDate isoToLocalDate = LocalDate.parse("1991-11-19");
     System.out.println("isoToLocalDate " + isoToLocalDate);
 
-    LocalDateTime localDateTime = LocalDateTime.now(); // Data local com hora e sem fuso horário
+    // Data local com hora e sem fuso horário
+    LocalDateTime localDateTime = LocalDateTime.now();
     System.out.println("localDateTime " + localDateTime);
 
-    LocalDateTime isoToLocalDateTime = LocalDateTime.parse("1991-11-19T15:00"); // De ISO para data local com hora e sem fuso horário
+    // De ISO para data local com hora e sem fuso horário
+    LocalDateTime isoToLocalDateTime = LocalDateTime.parse("1991-11-19T15:00");
     System.out.println("isoToLocalDateTime " + isoToLocalDateTime);
 
-    ZonedDateTime zonedDateTime = ZonedDateTime.now(); // Data-hora global com fuso horário negativo
+    // Data-hora global com fuso horário negativo
+    ZonedDateTime zonedDateTime = ZonedDateTime.now();
     System.out.println("zonedDateTime " + zonedDateTime);
 
-    ZonedDateTime isoToZonedDateTime = ZonedDateTime.parse("1991-11-19T15:00-03:00"); // De ISO para data-hora global com fuso horário negativo
+    // De ISO para data-hora global com fuso horário negativo
+    ZonedDateTime isoToZonedDateTime = ZonedDateTime.parse("1991-11-19T15:00-03:00");
     System.out.println("isoToZonedDateTime " + isoToZonedDateTime);
 
-    ZonedDateTime zonedDateTimeId = ZonedDateTime.now(ZoneId.of("Asia/Magadan")); // Data-hora global com fuso horário positivo
+    // Data-hora global com fuso horário positivo
+    ZonedDateTime zonedDateTimeId = ZonedDateTime.now(ZoneId.of("Asia/Magadan"));
     System.out.println("zonedDateTimeId " + zonedDateTimeId);
 
-    ZonedDateTime isoToZonedDateTimeId = ZonedDateTime.parse("1991-11-19T15:00+12:00"); // De ISO para data-hora global com fuso horário positivo
+    // De ISO para data-hora global com fuso horário positivo
+    ZonedDateTime isoToZonedDateTimeId = ZonedDateTime.parse("1991-11-19T15:00+12:00");
     System.out.println("isoToZonedDateTimeId " + isoToZonedDateTimeId);
 
-    Instant instant = Instant.now(); // Data-hora global com fuso horário Z (Londres)
+    // Data-hora global com fuso horário Z (Londres)
+    Instant instant = Instant.now();
     System.out.println("instant " + instant);
 
-    Instant isoToInstant = Instant.parse("1991-11-19T15:00:24Z"); // De ISO para data-hora global com fuso horário Z (Londres)
+    // De ISO para data-hora global com fuso horário Z (Londres)
+    Instant isoToInstant = Instant.parse("1991-11-19T15:00:24Z");
     System.out.println("isoToInstant " + isoToInstant);
 
 //    Instant isoToInstantDifferentZone = Instant.parse("1991-11-19T15:00:24Z");
 //    System.out.println("isoToInstantDifferentZone " + isoToInstantDifferentZone);
 
+    // Texto customizado para data-hora
     DateTimeFormatter brazilianPattern = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     LocalDate brazilianToLocalDate = LocalDate.parse("19/11/1991", brazilianPattern);
     System.out.println("brazilianToLocalDate " + brazilianToLocalDate);
 
     DateTimeFormatter brazilianPatternWithHours = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-    LocalDateTime brazilianWithHoursToLocalDate = LocalDateTime.parse("19/11/1991 15:45", brazilianPatternWithHours);
+    LocalDateTime brazilianWithHoursToLocalDate =
+        LocalDateTime.parse("19/11/1991 15:45", brazilianPatternWithHours);
     System.out.println("brazilianWithHoursToLocalDate " + brazilianWithHoursToLocalDate);
 
     LocalDate isolatedLocalDate = LocalDate.of(1991, 11, 19);
     System.out.println("isolatedLocalDate " + isolatedLocalDate);
 
-    LocalDateTime isolatedLocalDateTime = LocalDateTime.of(1991, 11, 19, 15, 45);
+    LocalDateTime isolatedLocalDateTime =
+        LocalDateTime.of(1991, 11, 19, 15, 45);
     System.out.println("isolatedLocalDateTime " + isolatedLocalDateTime);
 
-    ZonedDateTime isolatedZonedDateTime = ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Asia/Magadan"));
+    ZonedDateTime isolatedZonedDateTime =
+        ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Asia/Magadan"));
     System.out.println("isolatedZonedDateTime " + isolatedZonedDateTime);
+
+    // Data-hora para texto customizado
+    System.out.println(localDateTime.format(brazilianPattern));
+    System.out.println(localDateTime.format(brazilianPatternWithHours));
+    System.out.println(brazilianPattern.format(localDate));
+
+    // Instant para texto customizado
+    DateTimeFormatter customInstant = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+        .withZone(ZoneId.systemDefault());
+    System.out.println("custom instant " + customInstant.format(instant));
+
+//    for (String zoneId : ZoneId.getAvailableZoneIds()) {
+//      System.out.println(zoneId);
+//    }
+
+    // Data-hora global para local
+    LocalDate instantToLocalDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+    LocalDateTime instantToLocalDateTime = LocalDateTime.ofInstant(Instant.now(), ZoneId.of("Portugal"));
+    System.out.println("instantToLocalDate " + instantToLocalDate);
+    System.out.println("instantToLocalDateTime " + instantToLocalDateTime);
+
+    // Valores específicos da data
+    System.out.println("Dia " + localDate.getDayOfMonth());
+    System.out.println("Mês (nome) " + localDate.getMonth());
+    System.out.println("Mês (número) " + localDate.getMonth());
+    System.out.println("Ano " + localDate.getYear());
+    System.out.println("Dia na contagem geral do ano (1 a 365) " +
+        LocalDate.of(2023, 01, 01).getDayOfYear());
+    System.out.println("Dia na contagem geral do ano (1 a 365) " +
+        LocalDate.of(2023, 12, 31).getDayOfYear());
+    System.out.println("Hora " + localDateTime.getHour());
+    System.out.println("Minuto " + localDateTime.getMinute());
+    System.out.println("Segundo " + localDateTime.getSecond());
+    System.out.println("Milissegundo " + localDateTime.getNano());
   }
+
 }
